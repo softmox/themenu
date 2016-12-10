9a0a2131d5dbf99a5918ecb5b02464875e19a8b7
scott <scott@cogolabs.com>
2016-12-10 14:37:20 -0500

    change debug for deploy

--- themenu/settings.py
+++ themenu/settings.py
@@ -24,8 +24,8 @@ print PROJECT_ROOT
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get('DEBUG') or False  # For use on heroku to run in debug sometimes
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG') or False  # For use on heroku to run in debug sometimes
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG is False:
