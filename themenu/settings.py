024afb6f8c0f7a2a62b848109de2a72c0ae15d4b
scott <scott@cogolabs.com>
2016-12-10 17:17:53 -0500

    add django extensions

--- themenu/settings.py
+++ themenu/settings.py
@@ -34,6 +34,7 @@ else:
 
 INSTALLED_APPS = [
     'themenu.apps.ThemenuConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
