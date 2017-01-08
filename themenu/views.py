e774d387e50102be18916f2c2030e44529ec88db
alpha-tango <atpope5@gmail.com>
2017-01-08 16:54:28 -0500

    + update ingredients as well

--- themenu/views.py
+++ themenu/views.py
@@ -180,7 +180,7 @@ class TagListView(ListView):
 
 class DishUpdateView(UpdateView):
     model = Dish
-    fields = ['name', 'notes', 'source', 'recipe', 'speed', 'ease', 'results']
+    fields = ['name', 'notes', 'source', 'recipe', 'speed', 'ease', 'results', 'ingredients']
 
     def get_success_url(self):
         return reverse('dish-detail', kwargs={'pk':self.object.id})
