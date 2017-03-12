51054951c3d86c1d478881fb7c6880bb40b3f376
scott <scott@cogolabs.com>
2017-03-12 14:40:26 -0400

    add random grocery to admin

--- themenu/admin.py
+++ themenu/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Tag, Ingredient, Dish, Meal, Course, MyUser, Team
+from .models import (
+    Tag,
+    Ingredient,
+    Dish,
+    Meal,
+    Course,
+    MyUser,
+    Team,
+    RandomGroceryItem
+)
 
 
 class CourseInline(admin.TabularInline):
@@ -22,3 +31,4 @@ admin.site.register(Dish, DishAdmin)
 admin.site.register(Meal, MealAdmin)
 admin.site.register(MyUser)
 admin.site.register(Team)
+admin.site.register(RandomGroceryItem)
