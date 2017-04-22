0e9a7c4b91562bc7684ccb02b97eadcd63c281c3
alpha-tango <atpope5@gmail.com>
2017-04-22 13:44:16 -0400

    + fix #61

--- themenu/models.py
+++ themenu/models.py
@@ -11,7 +11,7 @@ def randcolor():
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def get_absolute_url(self):
         return reverse('team-detail', args=[str(self.id)])
