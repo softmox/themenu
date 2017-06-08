a2c1493394b4521e2594fb4df861d4a0dd20bd9b
scott <scott@cogolabs.com>
2017-06-07 23:23:16 -0400

    update prepared/eaten to purchased, close #30
    added function for course grocery fetch to break this up

--- themenu/signals.py
+++ themenu/signals.py
@@ -15,6 +15,11 @@ def get_course_ingredients(course):
     return (ingredient for ingredient in course.dish.ingredients.all())
 
 
+def get_course_groceries(course):
+    ingredient_ids = [ingredient.id for ingredient in get_course_ingredients(course)]
+    return GroceryListItem.objects.filter(course=course, ingredient__in=ingredient_ids)
+
+
 @receiver(post_save, sender=Course)
 def notify_course_post_save(sender, **kwargs):
     course = kwargs['instance']
@@ -29,6 +34,7 @@ def notify_course_post_save(sender, **kwargs):
                 GroceryListItem(course=course, ingredient=ingredient).save()
     else:
         if 'eaten' in update_fields or 'prepared' in update_fields:
+            get_course_groceries(course).update(purchased=True)
             return
 
         # Delete the grocery items for this course
