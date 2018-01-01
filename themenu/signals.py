4b6689a3e7afff29fb21e982943953aca46377b0
scott <scott.stanie@gmail.com>
2018-01-01 15:22:03 -0600

    change signals to use ingredient_amount

--- themenu/signals.py
+++ themenu/signals.py
@@ -1,23 +1,19 @@
 from django.conf import settings
-from django.db.models.signals import (
-    pre_save,
-    post_save,
-    m2m_changed,
-    pre_delete
-)
+from django.db.models.signals import post_save
 
 from django.dispatch import receiver
 
 from .models import Course, GroceryListItem, MyUser
 
 
-def get_course_ingredients(course):
-    return (ingredient for ingredient in course.dish.ingredients.all())
+def get_course_ingredient_amounts(course):
+    return (ingredient_amount for ingredient_amount
+            in course.dish.ingredient_amounts.all())
 
 
 def get_course_groceries(course):
-    ingredient_ids = [ingredient.id for ingredient in get_course_ingredients(course)]
-    return GroceryListItem.objects.filter(course=course, ingredient__in=ingredient_ids)
+    ingredient_ids = [ing_amt.id for ing_amt in get_course_ingredient_amounts(course)]
+    return GroceryListItem.objects.filter(course=course, ingredient_amount__in=ingredient_ids)
 
 
 @receiver(post_save, sender=Course)
@@ -30,11 +26,11 @@ def notify_course_post_save(sender, **kwargs):
 
     if kwargs['created'] is True:
         # Make new ones
-        for ingredient in get_course_ingredients(course):
+        for ing_amt in get_course_ingredient_amounts(course):
             try:
-                GroceryListItem.objects.get(course=course, ingredient=ingredient)
+                GroceryListItem.objects.get(course=course, ingredient_amount=ing_amt)
             except GroceryListItem.DoesNotExist:
-                GroceryListItem(course=course, ingredient=ingredient).save()
+                GroceryListItem(course=course, ingredient_amount=ing_amt).save()
     else:
         if 'eaten' in update_fields or 'prepared' in update_fields:
             get_course_groceries(course).update(purchased=True)
@@ -42,11 +38,11 @@ def notify_course_post_save(sender, **kwargs):
 
         # Delete the grocery items for this course
         new_grocery_id_list = []
-        for ingredient in get_course_ingredients(course):
+        for ing_amt in get_course_ingredient_amounts(course):
             try:
-                GroceryListItem.objects.get(course=course, ingredient=ingredient)
+                GroceryListItem.objects.get(course=course, ingredient_amount=ing_amt)
             except GroceryListItem.DoesNotExist:
-                g = GroceryListItem(course=course, ingredient=ingredient)
+                g = GroceryListItem(course=course, ingredient_amount=ing_amt)
                 g.save()
                 # Keep track of the ones we just added
                 new_grocery_id_list.append(g.id)
